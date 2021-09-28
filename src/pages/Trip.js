1be671da4e8b720cf5cbeb4eb407b7cde0a374ad
bettercallazamat <bettercallazamat@gmail.com>
2021-09-28 23:40:35 +0600

    Redirect to 404 on wrong trip url

--- src/pages/Trip.js
+++ src/pages/Trip.js
@@ -1,18 +1,25 @@
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import TripDetails from '../containers/TripDetails';
 
 const Trip = () => {
   const trips = useSelector((state) => state.trips);
   const { id } = useParams();
 
+  const tripSelector = (id) => {
+    const trip = trips.tripsCollection.filter((trip) => trip.id === parseInt(id, 10));
+    return (
+      trip[0]
+        ? trip.map((trip) => (<TripDetails trip={trip} key={trip.id} />))
+        : <Redirect to="/Page404" />
+    );
+  };
+
   return (
     <>
-      { trips.loading
+      {trips.loading
         ? <span className="loading">Loading...</span>
-        : trips.tripsCollection
-          .filter((trip) => trip.id === parseInt(id, 10))
-          .map((trip) => (<TripDetails trip={trip} key={trip.id} />)) }
+        : tripSelector(id)}
     </>
   );
 };
