873fb16becf0486b590e8f3800b765713377355a
bettercallazamat <bettercallazamat@gmail.com>
2021-10-13 22:43:15 +0600

    Change layout

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,15 +1,28 @@
 import { Link } from 'react-router-dom';
 import UserProfile from '../components/UserProfile';
 import logo from '../assets/images/logo.png';
+import githubIcon from '../assets/images/github-icon.png';
+import linkedinIcon from '../assets/images/linkedin-icon.png';
 import '../assets/styles/Nav.css';
 
 const Nav = () => (
   <div className="NavWrapper">
     <nav className="nav">
-      <Link to="/" title="Home Page">
-        <img alt="logo" src={logo} />
-      </Link>
-      <UserProfile />
+      <div className="main">
+        <Link to="/" title="Home Page">
+          <img alt="logo" src={logo} />
+        </Link>
+        <UserProfile />
+      </div>
+      <div className="sections">
+        <Link to="/" title="Home Page">
+          <p>Trips</p>
+        </Link>
+      </div>
+      <div className="socials">
+        <a href="https://github.com/bettercallazamat"><img src={githubIcon} alt="github" /></a>
+        <a href="https://www.linkedin.com/in/azamat-nuriddinov/"><img src={linkedinIcon} alt="linkedin" /></a>
+      </div>
     </nav>
   </div>
 );
